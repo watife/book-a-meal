05c51f4a8ecfa0dda2167e2cbcb678f700930325
Branndit-03 <user1@Users-MacBook-Pro.local>
2019-02-15 22:46:18 +0100

    some services for meals

---
+++ api/utils/dummyData.js
@@ -0,0 +1,28 @@
+export default {
+  meals: [
+    {
+      id: 1,
+      name: "Fried Rice",
+      size: "Medium",
+      prize: "450"
+    },
+    {
+      id: 2,
+      name: "Jollof Rice",
+      size: "Large",
+      prize: "550"
+    },
+    {
+      id: 3,
+      name: "Beans",
+      size: "small",
+      prize: "150"
+    },
+    {
+      id: 4,
+      name: "Eba and Semo",
+      size: "Large",
+      prize: "300"
+    }
+  ]
+};
